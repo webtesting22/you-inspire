783844bdd5f8a6afafbfafe7c0c7380748823ab7
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-02-13 17:05:10 +0530

    Mobile freindly CSS Added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,29 +4,78 @@ import { Row, Col } from "antd";
 const Footer = () => {
     return (
         <>
-            <section>
-                <div id="FooterContentContainer">
+            <section id="FooterContentContainer">
+                <div className="PaddingAdjust">
                     <div className="FooterHeaderPart">
-                        <div>
+                        <div style={{ display: "flex", justifyContent: "center" }}>
                             <p className="fontFamily">Popular Popular</p>
                         </div>
-                        <Row>
+                        {/* <Row>
                             <Col lg={8}>
                                 <div>
-
+                                    <h2 className="fontFamilyAnother Mediumheading">Popular Name</h2>
                                 </div>
                             </Col>
                             <Col lg={8}>
                                 <div>
-
+                                    <h2 className="fontFamilyAnother Mediumheading">Popular Name</h2>
                                 </div>
                             </Col>
                             <Col lg={8}>
                                 <div>
-
+                                    <h2 className="fontFamilyAnother Mediumheading">Popular Name</h2>
                                 </div>
                             </Col>
-                        </Row>
+                        </Row> */}
+                    </div>
+                    <div className="Topheadingbrandtitle">
+                        <div>
+                            <span className="fontFamilyAnother Mediumheading">You Inspire</span>
+                        </div>
+                    </div>
+                    <br /><br />
+                    <div>
+                        <div className="FooterinsideBottomContainer">
+                            <Row>
+                                <Col lg={6}>
+                                    <div>
+                                        <p className="fontFamilyAnother smallHeading">Navigate</p>
+                                        <div>
+                                            <ul>
+                                                <li>Home</li>
+                                                <li>About</li>
+
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col lg={12}>
+                                    <div>
+                                        <div className="ImageAttachContainer">
+                                            <img src="https://images.unsplash.com/photo-1586894608265-bc4474cc34ae?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                                            <img src="https://images.unsplash.com/photo-1685293943609-36ccbb9fe84c?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col lg={6}>
+                                    <div>
+                                        <div>
+                                            <p className="fontFamilyAnother smallHeading">Social Connects</p>
+                                            <div>
+                                                <ul>
+                                                    <li>Instagram</li>
+                                                    <li>Linkedin</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                    <br /><br />
+                    <div className="CopywriteText">
+                        <span>DESIGNED BY Outlead Solutions</span>
                     </div>
                 </div>
             </section>
